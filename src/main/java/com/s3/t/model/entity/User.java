550b91ba1ad387e4fcd821c4859eb210efb9f5b7
Marcos <69874820+marcosep192000@users.noreply.github.com>
2022-08-03 19:09:27 -0300

    prueba marcos

--- src/main/java/com/s3/t/model/entity/User.java
+++ src/main/java/com/s3/t/model/entity/User.java
@@ -1,4 +1,30 @@
 package com.s3.t.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.asm.Advice;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
