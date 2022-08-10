b663b76d58597130a87068979b2fc1e9a4f0c7aa
unknown <marcosep192000@hotmail.com>
2022-08-09 22:07:32 -0300

    create roles

---
+++ src/main/java/com/s3/t/model/entity/Role.java
@@ -0,0 +1,35 @@
+package com.s3.t.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
+@Table(name="roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String description;
+    @CreationTimestamp
+    private Timestamp timestamp;
+
+
+    public Role(Long id, String name, String description, Timestamp timestamp) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.timestamp = timestamp;
+    }
+
+    public Role() {
+    }
+}
