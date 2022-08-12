e84956c15a20221373380e7d27028b3033f553e1
Abel3581 <marzoa3581@gmail.com>
2022-08-12 11:18:57 -0300

    config swagger

--- src/main/java/com/s3/t/config/SecurityConfig.java
+++ src/main/java/com/s3/t/config/SecurityConfig.java
@@ -3,5 +3,16 @@ package com.s3.t.config;
 public class SecurityConfig {
 
 
+    private static final String[] publicEndpoint = {
+            "/swagger-resources/**",
+            "/swagger-ui/**", "/v2/api-docs",
+            "/v3/api-docs",
+            "/api/docs",
+            "/api/docs/**",
+            "/api/docs/swagger-ui",
+            "/swagger-ui.html",
+            "/**/swagger-ui/**",
+            "/swagger-ui"
+    };
 
 }
