a3b7729cbf7dd59b9890c6b8ba929fc6979f6572
Abel3581 <marzoa3581@gmail.com>
2022-08-17 19:24:03 -0300

    agregado endpoint save localidad

---
+++ src/main/java/com/s3/t/service/abstraction/LocationService.java
@@ -0,0 +1,8 @@
+package com.s3.t.service.abstraction;
+
+import com.s3.t.model.request.LocationRequest;
+import com.s3.t.model.response.LocationResponse;
+
+public interface LocationService {
+    LocationResponse save(LocationRequest request);
+}
